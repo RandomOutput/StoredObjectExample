853ca3b42451cf499cef97d9bd05d2b65b0e3e89
RandomOutput <dpx109@gmail.com>
2013-05-09 16:42:42 -0700

    quick edit to the stars example to show StoredObject

---
+++ main.cpp
@@ -0,0 +1,67 @@
+/*
+ * Sifteo SDK Example.
+ */
+
+#include <sifteo.h>
+#include "assets.gen.h"
+using namespace Sifteo;
+
+static const unsigned gNumCubes = 1;
+
+static AssetSlot MainSlot = AssetSlot::allocate()
+    .bootstrap(GameAssets);
+
+static Metadata M = Metadata()
+    .title("Stored Object Example")
+    .package("com.sifteo.sdk.StoredObject", "1.0")
+    .icon(Icon)
+    .cubeRange(gNumCubes);
+
+/*
+This code example will store two values in stored objects the first time the progam is run. 
+Once you quit and restart the program, it will then retreive the values and print them back out.
+*/
+
+void main()
+{
+    StoredObject saveDataOne(188); 
+
+    /*
+    Ok so the above line creates a reference to a stored object location. 
+    It gives us the ability to read and write save data to somewhere we can retrieve it.
+
+    We do this with key value pairs. Think of a dictionary where words reference 
+    defentitions. 
+
+    Word => Defention
+    Car => A four wheeled vehicle.
+    Key => Value
+
+    In this case we're creating a StoredObject with the key '188'
+    */
+
+    StoredObject saveDataTwo(187); //This is our second stored object. It's key is 187.
+
+    int x = 1; //this is the value we'll store in breadcrumb
+    int y = 10; //this is the value we'll store in breadcrumb
+
+    if (saveDataOne.readObject(x) <= 0) {
+        LOG("no save data - writing some\n");
+        saveDataOne.writeObject(x); //Write the value '1' to saveDataOne (whose key is 188)
+        saveDataTwo.writeObject(y); //Write the value '10' to saveDataTwo (whose key is 187)
+        LOG("Save data written. Restart the app now.\nWarning, reinstalling the app will wipe this data\nUse the base to restart the app");
+    } else {
+        int x_prime = 0;
+        int y_prime = 0;
+
+        saveDataOne.readObject(x_prime); //read the data in saveDataOne and assign it to x_prime
+        saveDataTwo.readObject(y_prime); //read the data in saveDataOne and assign it to y_prime
+        LOG("saveDataOne = %i\n", x_prime);
+        LOG("saveDataTwo = %i\n", y_prime);
+    }
+
+    while (1) {
+        System::paint();
+    }
+    
+}
---
+++ main.d
@@ -0,0 +1,115 @@
+main.o: main.cpp \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/syscall.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/types.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/audio.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/events.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/vram.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/asset.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/elf.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/array.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/memory.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/macros.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/group.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/limits.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/math.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/loader.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/audio.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/image.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/audio.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/cube.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/event.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/filesystem.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/metadata.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/motion.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/string.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/system.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/time.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/color.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/sprite.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/framebuffer.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg0rom.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg0.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg1.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg2.h \
+  /Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/tilebuffer.h \
+  assets.gen.h
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/syscall.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/types.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/audio.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/events.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/vram.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/asset.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/abi/elf.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/array.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/memory.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/macros.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/group.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/limits.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/math.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/loader.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/audio.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/asset/image.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/audio.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/cube.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/event.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/filesystem.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/metadata.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/motion.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/string.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/system.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/time.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/color.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/sprite.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/framebuffer.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg0rom.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg0.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg1.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/bg2.h:
+
+/Users/RandomOutput/Developer/Sifteo/SDK/sifteo-sdk-mac64-v1.0.0-71-g0792e5f/include/sifteo/video/tilebuffer.h:
+
+assets.gen.h:
